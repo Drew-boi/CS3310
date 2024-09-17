7a5e33f4806f6374c3c18c271bac9fb4ad63bf9d
Drew-boi <122581219+Drew-boi@users.noreply.github.com>
2024-09-16 20:43:09 -0600

    Using the correct list 🤓

--- Inversions/CountingInversions.py
+++ Inversions/CountingInversions.py
@@ -16,8 +16,6 @@ def merge(lyst1 : list, lyst2 : list ) -> list:
                 inversions += m - k # Every number after lyst1[k] is also an inversion
                 returnList.append(lyst2[j])
                 j += 1 
-            else:
-                continue
         
         elif k < m:
             returnList.append(lyst1[k])
@@ -51,17 +49,16 @@ def main():
         test_lists = []
         for line in f:
             line = line.strip()
-            char_array = [c for c in line]
-            test_lists.append(char_array)
-    for test_list in test_lists:
-        # print(f"Unsorted List: {test_list}", end="   ") # use this line if you want to check the unsorted list
-        sorted_list, inversions = mergesort(test_list)
-        print(f"Sorted list: {sorted_list}, Inversions: {inversions}")
+            test_lists.append(int(line))
+        #print(f"Unsorted List: {test_lists}", end="   ") # use this line if you want to check the unsorted list
+        sorted_list, inversions = mergesort(test_lists)
+        # Sorted list: {sorted_list} \n
+        print(f"Inversions: {inversions}")
     
     # Test another case
-    lyst = [1, 4, 6, 3, 2, 9, 5, 12, 10, 11, 7, 13, 8, 15, 14]
+    lyst = [5, 3, 1, 7, 8, 4, 9, 2, 11, 8]
     sorted_list, inversions = mergesort(lyst)
-    print(f"Sorted list: {sorted_list}, Inversions: {inversions}")
+    #print(f"Sorted list: {sorted_list}, Inversions: {inversions}")
 
 
 if __name__ == '__main__':
